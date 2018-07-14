aa057f7958e8ab362ab311b35689994fe4dfb258
Charlie Jaime <baroonie10@gmail.com>
2018-07-14 05:00:15 -0700

    tuned car turning speed and stopped it from turning in place

--- js/Car.js
+++ js/Car.js
@@ -7,6 +7,7 @@ const GROUNDSPEED_DECAY_MULT = 0.97;
 const DRIVE_POWER = 0.3;
 const REVERSE_POWER = 0.3;
 const TURN_RATE = 0.06;
+const MIN_SPEED_TO_TURN = 0.5;
 
 var carAng = 0;
 
@@ -33,11 +34,13 @@ function carMove(){
   if (keyHeld_Reverse) {
     carSpeed -= REVERSE_POWER;
   }
-  if (keyHeld_TurnLeft) {
-    carAng -= TURN_RATE;
-  }
-  if (keyHeld_TurnRight) {
-    carAng += TURN_RATE;
+  if (Math.abs(carSpeed) > MIN_SPEED_TO_TURN) {
+    if (keyHeld_TurnLeft) {
+      carAng -= TURN_RATE;
+    }
+    if (keyHeld_TurnRight) {
+      carAng += TURN_RATE;
+    }
   }
 
   carX += Math.cos(carAng) * carSpeed;
