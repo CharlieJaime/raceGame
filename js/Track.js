b88fe02c65d5e5b703c520653e9924ec29e27b6c
Charlie Jaime <baroonie10@gmail.com>
2018-07-14 04:40:07 -0700

    added track consts for new imgs and changed isObstacle

--- js/Track.js
+++ js/Track.js
@@ -4,7 +4,7 @@ const TRACK_GAP = 2;
 const TRACK_COLS = 20;
 const TRACK_ROWS = 15;
 var trackGrid = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 4, 0, 5, 0, 1,
                   1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
                   1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
                   1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -22,12 +22,15 @@ var trackGrid = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 const TRACK_ROAD = 0;
 const TRACK_WALL = 1;
 const TRACK_PLAYERSTART = 2;
+const TRACK_GOAL = 3;
+const TRACK_FLAG = 4;
+const TRACK_TREE = 5;
 
-function isWallAtColRow(col, row){
+function isObstacleAtColRow(col, row){
   if (col >= 0 && col < TRACK_COLS &&
       row >= 0 && row < TRACK_ROWS) {
     var trackIndexUnderCoord= rowColToArrayIndex(col, row);
-    return (trackGrid[trackIndexUnderCoord] == TRACK_WALL);
+    return (trackGrid[trackIndexUnderCoord] != TRACK_ROAD);
   } else{
     return false;
   }
@@ -38,7 +41,7 @@ function carTrackColl(){
   var carTrackRow = Math.floor(carY / TRACK_H);
   var trackIndexUnderCar = rowColToArrayIndex(carTrackCol, carTrackRow);
   if (carTrackCol >= 0 && carTrackCol < TRACK_COLS && carTrackRow >= 0 && carTrackRow < TRACK_ROWS){
-    if (isWallAtColRow(carTrackCol, carTrackRow)) {
+    if (isObstacleAtColRow(carTrackCol, carTrackRow)) {
       carX -= Math.cos(carAng) * carSpeed;
       carY -= Math.sin(carAng) * carSpeed;
 
