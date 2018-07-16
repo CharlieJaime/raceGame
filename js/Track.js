cb70de60461b9ff65436e02ae6bf7bc3ff815709
Charlie Jaime <baroonie10@gmail.com>
2018-07-16 01:12:22 -0700

    optimizied track drawing loops

--- js/Track.js
+++ js/Track.js
@@ -56,15 +56,23 @@ function rowColToArrayIndex(col, row){
 }
 
 function drawtracks(){
+
+  var arrayIndex = 0;
+  var drawTileX = 0;
+  var drawTileY = 0;
+
+
   for (var eachRow=0; eachRow<TRACK_ROWS; eachRow++) {
     for(var eachCol=0; eachCol<TRACK_COLS; eachCol++){
-
-      var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
       var tileKind = trackGrid[arrayIndex];
       var useImg = trackPics[tileKind];
 
-      canvasContext.drawImage(useImg, TRACK_W*eachCol, TRACK_H*eachRow);
+      canvasContext.drawImage(useImg, drawTileX, drawTileY);
 
+      drawTileX += TRACK_W;
+      arrayIndex++;
     }// each track col
+    drawTileX = 0;
+    drawTileY += TRACK_H;
   }// each trackrow
 }// drawtracks
